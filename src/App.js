84886f4b26669b4f24821da085653584955845f8
hiepdv99 <57650927+hiepdv99@users.noreply.github.com>
2021-12-15 22:22:08 +0700

    update login

--- src/App.js
+++ src/App.js
@@ -1,28 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
-import Nav from './layouts/Nav.js';
+import Nav from './layouts/Nav';
 import Footer from './layouts/Footer';
-import Card from './layouts/Card';
-import DieuhuongURL from './routers/DieuhuongURL.js'
-import axios from 'axios';
-function App() {
-  return (
-    <div className="App">
-      <div className="App">
-      <Nav></Nav>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <DieuhuongURL/>
-      <br/>
-      <br/>
-      <Footer></Footer>
-      
+import React from 'react';
+import jwt_decode from "jwt-decode";
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Contact from "./componet/contact/Contact"
+import Home from "./views/Home"
+import FormRe from "./componet/formRe/FormRe"
+import Login from "./views/Login"
+import Reg from "./views/Reg"
+import Page500 from "./views/page500"
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    let token = localStorage.getItem('accessToken');
+    let user = null
+    if (token) {
+      try {
+        user = jwt_decode(token)
+      } catch (err) {
+        console.log(err)
+      }
+    }
+    this.state = {
+      user: user,
+      token: token
+    }
+  }
+  setToken = (newValue) => {
+    this.setState({ 'token': newValue, 'user': (newValue ? jwt_decode(newValue) : null) })
+  }
+  render() {
+    return (
+      <div className="wrapper">
+        <BrowserRouter>
+          <div className='content-wrapper'>
+          <Nav key={window.location.pathname} user={this.state.user} setToken={this.setToken}></Nav>
+            <Switch>
+              
+              <Route path="/login" render={(props) => <Login setToken={this.setToken} user={this.state.user} {...props} />} />
+              <Route path="/home" render={(props) => <Home user={this.state.user} {...props} />} />
+              <Route path="/500" render={(props) => <Page500  {...props} />} />
+              <Route path="/contact" render={(props) => <Contact  {...props} user={this.state.user} {...props} />} />
+              <Route path="/form" render={(props) => <FormRe  {...props} user={this.state.user} {...props} />} />
+              <Route path="/reg" render={(props) => <Reg  {...props} user={this.state.user} {...props} />} />
+              {/* <Route path="/:unknown">
+                <Page404 />
+              </Route> */}
+
+              <Route path="/">
+                <Home />
+              </Route>
+            </Switch>
+            <Footer></Footer>
+          </div>
+        </BrowserRouter>
+        <Footer user={this.state.user} />
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/layouts/Nav.js
+++ src/layouts/Nav.js
@@ -1,6 +1,39 @@
 import React, { Component } from "react";
+import api from "./../Api/api"
 class Nav extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            num: 0,
+            notifications: []
+
+        }
+    }
     render() {
+        var user = this.props.user
+        var logout, login, register, userProfile = ''
+        var sepLogin = ''
+        if (user != null) {
+
+            userProfile =<a className="btn dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <i className="fa fa-user" style={{ "fontSize": "24px" }}></i> &nbsp;<span>{user.username}</span> &nbsp;
+                </a>;
+
+
+            logout =
+                <a className="dropdown-item" href="#" onClick={this.logOut}>
+                    <p><i className="fas fa-sign-out-alt"></i>&nbsp; Đăng xuất</p>
+                </a>
+        } else {
+            login = <a href='/login' className="login no-user">
+                <p>Login</p>
+            </a>;
+            register = <a href='/reg' className="register no-user">
+                <p>Register</p>
+            </a>
+
+            sepLogin = <div className="seperator no-user"></div>
+        }
         return (
             <div>
                 <header id="header herp" className="fixed-top header-transparent">
@@ -9,13 +42,17 @@ class Nav extends Component {
                             <ul>
                                 <li><a className="nav-link scrollto active" href="/home">Home</a></li>
                                 <li><a className="nav-link scrollto" href="/checkin">Visualization</a></li>
-                                <li><a className="nav-link scrollto" href="/login">Login</a></li>
-                                        
-                                
+
+
                                 <li><a className="nav-link scrollto" href="/contact">About Us</a></li>
+                                <li>{login}</li>
+                                <li>{sepLogin}</li>
+                                <li>{register}</li>
+                                <li>{userProfile}</li>
+                                <li>{logout}</li>
                             </ul>
                             <i className="bi bi-list mobile-nav-toggle" />
-                        </nav>{/* .navbar */}
+                        </nav>
                     </div>
                 </header>
                 <section id="hero">
@@ -27,6 +64,15 @@ class Nav extends Component {
                 </section>
             </div>
         );
+    }
+    logOut = async () => {
+        localStorage.removeItem('accessToken')
+        try {
+            await api.logout()
+        } catch (err) {
+            console.log(err)
+        }
+        window.location.replace("/");
     }
 }
 export default Nav;
